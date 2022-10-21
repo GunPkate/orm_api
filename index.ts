d88403936576e57b0151a36e667523e5765fa222
GunPKate <gun@builk.com>
2022-10-21 15:31:31 +0700

    cronjob message and time format

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import user from "./Routes/user";
 import mailer from "./Routes/mailer"
 import cron = require('cron')
 const cronjob = cron.CronJob
+import Handlebars from "handlebars"
+import moment from 'moment'
 
 const port:Number = 3000;
 const app:Application = express();
@@ -20,7 +22,7 @@ app.use('/api/mailer',mailer)
 var job = new cronjob(
 	'* * * * * *',
 	async function() {
-		console.log('You will see this message every second');
+		test()
         await app.use('/api/mailer',mailer)
 	},
 	null,
@@ -28,4 +30,26 @@ var job = new cronjob(
 	'America/Los_Angeles'
 );
 
+function test():void {
+var source =
+  "<p>เรียน {{name}}" +
+  "{{name}} สามารถเข้าระบบ เพื่อรายงานผลตัวชี้วัด และ ในไตรมาสที่ 1 ประจำปีงบประมาณ 2565 ภายในวันที่ 1 ธันวาคม พ.ศ. 2565 ถึงวันที่ 31 ธันวาคม พ.ศ. 2565" +
+  "จึงเรียนมาเพื่อทราบ" +
+  "งานฝ่ายแผนและงบประมาณ" +
+  "ข้อมูลการติดต่อ" +
+  "{{emp_name}}" +
+  "โทร.{{tel}}" +
+  "Email:" +
+  "<ul>{{#emails}} <li>{{name}}</li>{{/emails}} </ul>";
+var template = Handlebars.compile(source);
+
+var data = {
+  name: "วิทยาการคอมพิวเตอร์",
+  emp_name: "สมชาย เอ",
+  tel: "0821114544",
+  emails: [{ name: "g1@gmail.com" }, { name: "a1@gmail.com" }],
+};
+var result = template(data);
+console.log(result,moment().format("DD-MM-YYYY hh:mm:ss"));
+}
 app.listen(port,()=>console.log(`start server port: ${port}`))
